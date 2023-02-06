80588e00c90f0f4826c57ed0f69637b39b22e0eb
op-6160 <sagvd01@gmail.com>
2023-02-06 21:12:21 +0900

    크기조절을 넣었는데 1프레임만 적용되는거 같다

--- mp4_to_gif.py
+++ mp4_to_gif.py
@@ -1,7 +1,11 @@
 import os
 import imageio
+from scipy.misc import imresize
 
-def convert_to_gif(input_folder, output_folder):
+def resize_frame(frame, size):
+    return imresize(frame, size)
+
+def convert_to_gif(input_folder, output_folder, size):
     for filename in os.listdir(input_folder):
         if filename.endswith(".mp4"):
             mp4_file = os.path.join(input_folder, filename)
@@ -9,6 +13,8 @@ def convert_to_gif(input_folder, output_folder):
             with imageio.get_writer(gif_file, mode='I') as writer:
                 reader = imageio.get_reader(mp4_file)
                 for frames in reader:
+                    resized_frame = resize_frame(frames, size)
+                    writer.append_data(resized_frame)
                     writer.append_data(frames)
             print("Converted " + mp4_file + " to " + gif_file)
         else:
@@ -16,7 +22,11 @@ def convert_to_gif(input_folder, output_folder):
     print("done")
 
 #input_folder = input("Enter the path of the input folder: ")
+#output_folder = input("Enter the path of the output folder: ")
+#width = int(input("Enter the width of the output gif (in pixels): "))
+#height = int(input("Enter the height of the output gif (in pixels): "))
 input_folder = "D:/Users/sagvd17/mp4_to_gif/input_mp4"
 output_folder = "D:/Users/sagvd17/mp4_to_gif/output_gif"
-#output_folder = input("Enter the path of the output folder: ")
-convert_to_gif(input_folder, output_folder)
+width = 100
+height = 100
+convert_to_gif(input_folder, output_folder, (width, height))
