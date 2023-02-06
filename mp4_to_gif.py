d1476b0283500d25c0a653c567276c9d60a4a498
op-6160 <sagvd01@gmail.com>
2023-02-06 21:21:25 +0900

    잘된다 굿

--- mp4_to_gif.py
+++ mp4_to_gif.py
@@ -1,9 +1,7 @@
 import os
 import imageio
-from scipy.misc import imresize
-
-def resize_frame(frame, size):
-    return imresize(frame, size)
+from PIL import Image
+import numpy as np
 
 def convert_to_gif(input_folder, output_folder, size):
     for filename in os.listdir(input_folder):
@@ -13,20 +11,23 @@ def convert_to_gif(input_folder, output_folder, size):
             with imageio.get_writer(gif_file, mode='I') as writer:
                 reader = imageio.get_reader(mp4_file)
                 for frames in reader:
-                    resized_frame = resize_frame(frames, size)
-                    writer.append_data(resized_frame)
-                    writer.append_data(frames)
+                    resized_frame = Image.fromarray(frames).resize(size)
+                    writer.append_data(np.array(resized_frame))
             print("Converted " + mp4_file + " to " + gif_file)
         else:
             print(filename + " is not a .mp4 file.")
     print("done")
 
-#input_folder = input("Enter the path of the input folder: ")
-#output_folder = input("Enter the path of the output folder: ")
+
+## 직접입력
 #width = int(input("Enter the width of the output gif (in pixels): "))
 #height = int(input("Enter the height of the output gif (in pixels): "))
-input_folder = "D:/Users/sagvd17/mp4_to_gif/input_mp4"
-output_folder = "D:/Users/sagvd17/mp4_to_gif/output_gif"
-width = 100
-height = 100
+
+## 이모지용
+width,height = 100,100
+
+input_folder = "D:/Users/"
+output_folder = "D:/Users/"
+#input_folder = input("Enter the path of the input folder: ")
+#output_folder = input("Enter the path of the output folder: ")
 convert_to_gif(input_folder, output_folder, (width, height))
